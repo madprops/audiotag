fb2f8e224d7897bff88e8f7bede651a2c8ab835e
Auric Vente <mprops@protonmail.com>
2022-01-29 08:44:26 -0600

    Single file mode and other mods

--- audiotag.py
+++ audiotag.py
@@ -2,6 +2,7 @@ import sys
 import glob
 import re
 import sty
+import signal
 from pathlib import Path
 from mutagen.flac import FLAC
 from mutagen.oggvorbis import OggVorbis
@@ -60,7 +61,7 @@ def show_menu(full_menu = False):
     change_value(args[0], a1)
 
   elif args[0] == "move":
-    move_track()
+    move_track(a1)
 
   elif args[0] == "rename":
     rename_files()
@@ -85,12 +86,19 @@ def clean_titles():
       new_title = title.replace("_", " ").title()
       if title != new_title:
         set_tag(audio, "title", new_title)
-        update_track(audio, False)
-        show_info("Cleaned", f"{title} to {new_title}")
+        show_info("Clean", f"{title} to {new_title}")
 
 # Move track positions by selecting 2 indexes
-def move_track():
-  n1 = int(input("Track Number: "))
+def move_track(n1):
+  if len(files) == 1:
+    n2 = input("New Track Number: ")
+    set_tag(get_audio_object(files[0]), "tracknumber", n2)
+    return
+  
+  if n1 == "":
+    n1 = input("Track Number: ")
+  n1 = int(n1)
+
   n2 = int(input("New Track Number: "))
 
   if n1 <= 0 or n1 > len(files):
@@ -109,7 +117,12 @@ def move_track():
 def change_value(prop, ans):
   w = prop.capitalize()
 
-  if ans == "":
+  if len(files) == 1:
+    t = input(f"New {w}: ")
+    changeone(0, prop, t)
+    return
+
+  if ans == "": 
     ans = input("Target (#, all): ")
 
   if ans.isnumeric():
@@ -139,7 +152,7 @@ def rename_files():
       old_name = p.name
       if p.name != new_name:
         p.rename(Path(p.parent, new_name))
-        show_info("Renamed", f"{old_name} to {new_name}")
+        show_info("Rename", f"{old_name} to {new_name}")
 
     startup()
 
@@ -151,14 +164,14 @@ def show_tracks():
 
   for i, file in enumerate(files):
     audio = get_audio_object(file)
+    track = get_tag(audio, "tracknumber")
     artist = get_tag(audio, "artist")
     album = get_tag(audio, "album")
     genre = get_tag(audio, "genre")
     title = get_tag(audio, "title")
-    index = i + 1
 
     print(f"\
-{sty.fg.blue}#{sty.fg.rs} {index} | \
+{sty.fg.blue}#{sty.fg.rs} {track} | \
 {sty.fg.blue}Artist:{sty.fg.rs} {artist} | \
 {sty.fg.blue}Album:{sty.fg.rs} {album} | \
 {sty.fg.blue}Genre:{sty.fg.rs} {genre} | \
@@ -167,35 +180,28 @@ def show_tracks():
 # Fill missing data on Track, Artist, Album, Genre, and Title
 def check_tracks():
   for file in files:
-    audio = get_audio_object(file)
-    set_tag_if_empty(audio, "tracknumber", "1")
-    set_tag_if_empty(audio, "artist", "Unknown")
-    set_tag_if_empty(audio, "album", "Unknown")
-    set_tag_if_empty(audio, "genre", "Unknown")
-    set_tag_if_empty(audio, "title", "Unknown")
+    set_tag_if_empty(file, "tracknumber", "1")
+    set_tag_if_empty(file, "artist", "Unknown")
+    set_tag_if_empty(file, "album", "Unknown")
+    set_tag_if_empty(file, "genre", "Unknown")
+    set_tag_if_empty(file, "title", "Unknown")
 
 # If value is empty fill it with a value
-def set_tag_if_empty(audio, field, value):
+def set_tag_if_empty(file, field, value):
+  audio = get_audio_object(file)
   if get_tag(audio, field) == "":
     set_tag(audio, field, value)
-    update_track(audio)
 
 # Update track numbers based on the indexes of the files list
 def update_tracknumbers():
+  if len(files) == 1: return
+
   for i, file in enumerate(files):
     audio = get_audio_object(file)
     tracknum = get_tag(audio, "tracknumber")
     n = str(i + 1)
     if tracknum != n:
       set_tag(audio, "tracknumber", n)
-      update_track(audio)
-
-# Save track data and show feedback
-def update_track(audio, feedback = True):
-  title = get_tag(audio, "title")
-  audio.save()
-  if feedback:
-    show_info("Updated", title)
 
 # Change the index of a list item
 def change_index(old_index, new_index):
@@ -211,7 +217,6 @@ def changeone(index, field, value):
   file = files[index]
   audio = get_audio_object(file)
   set_tag(audio, field, value)
-  update_track(audio)
 
 # Get proper audio object
 def get_audio_object(file):
@@ -234,6 +239,8 @@ def get_tag(audio, tag):
 # Set a tag from an audio object
 def set_tag(audio, tag, value):
   audio[tag] = [value]
+  audio.save()
+  show_info("Update", f"{tag} set to {value} in {Path(audio.filename).name}")
 
 # Sort by tracknumber once at startup
 def initial_sort():
@@ -243,10 +250,15 @@ def initial_sort():
 # Get audio files
 def get_files():
   global files
-  flacs = glob.glob(f"{sys.argv[1]}/*.flac")
-  ogg = glob.glob(f"{sys.argv[1]}/*.ogg")
-  mp3 = glob.glob(f"{sys.argv[1]}/*.mp3")
-  files = flacs + ogg + mp3
+  filespath = sys.argv[1]
+  p = Path(filespath)
+  if p.is_dir():
+    flacs = glob.glob(f"{filespath}/*.flac")
+    ogg = glob.glob(f"{filespath}/*.ogg")
+    mp3 = glob.glob(f"{filespath}/*.mp3")
+    files = flacs + ogg + mp3
+  elif p.is_file():
+    files = [str(p)]
   if len(files) == 0:
     print("No files found")
     quit()
@@ -260,6 +272,8 @@ def startup():
 
 # Main function
 def main() -> None:
+  signal.signal(signal.SIGINT, lambda x, y: sys.exit(0))
+  
   startup()
 
   while True:

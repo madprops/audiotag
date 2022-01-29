30dff1aa2d7f9e1a392ff4bf1c2c5954f644cd35
Auric Vente <mprops@protonmail.com>
2022-01-28 22:17:47 -0600

    Improvements

--- audiotag.py
+++ audiotag.py
@@ -4,58 +4,80 @@ import re
 import mutagen
 from pathlib import Path
 
-def show_menu():
-  print("")
-  print("title - Change a track's title")
-  print("artist - Change a track's artist")
-  print("artistall - Change the album's artist")
+# Show the full menu with descriptions
+def show_full_menu():
+  print("title - Change track titles")
+  print("artist - Change track artists")
+  print("album - Change track albums")
+  print("genre - Change track genres")
   print("move - Move track to a new position")
   print("rename - Apply filename changes")
+  print("help - Show this message")
+
+# Show a simple menu without descriptions
+def show_simple_menu(): 
+  print("title | artist | album | genre | move | rename | help")
+
+# Show the menu and wait for input
+def show_menu(full_menu = False):
+  print("")
+
+  if full_menu:
+    show_full_menu()
+  else:
+    show_simple_menu()
 
   ans = input("> ")
 
-  if ans == "title":
-    n = int(input("Track Number: "))
-    t = input("New Track Title: ")
-    if n <= 0 or n > len(files):
-      return
-    changeone(n - 1, "title", t)  
-
-  elif ans == "artist":
-    n = int(input("Track Number: "))
-    a = input("New Track Artist: ")
-    if n <= 0 or n > len(files):
-      return
-    changeone(n - 1, "artist", a)
-
-  elif ans == "artistall":
-    a = input("New Artist Name: ")
-    changeall("artist", a)   
+  if ans == "title" or ans == "artist" or ans == "album" or ans == "genre":
+    change_value(ans)      
 
   elif ans == "move":
-    n1 = int(input("Track Number: "))
-    n2 = int(input("New Track Number: "))
-
-    if n1 <= 0 or n1 > len(files):
-      return
-    
-    if n2 <= 0:
-      n2 = 1
-    
-    if n2 > len(files):
-      n2 = len(files)
-
-    change_index(n1 - 1, n2 - 1)
-    update_tracknumbers()
-    show_tracks()
+    move_track()
   
   elif ans == "rename":
     rename_files()
     quit()
   
+  elif ans == "help":
+    show_menu(True)
+  
   else:
     quit()
 
+# Move track positions by selecting 2 indexes
+def move_track():
+  n1 = int(input("Track Number: "))
+  n2 = int(input("New Track Number: "))
+
+  if n1 <= 0 or n1 > len(files):
+    return
+  
+  if n2 <= 0:
+    n2 = 1
+  
+  if n2 > len(files):
+    n2 = len(files)
+
+  change_index(n1 - 1, n2 - 1)
+  update_tracknumbers()
+
+# Generic function to change one or more values
+def change_value(prop):
+  w = prop.capitalize()
+  s = input("Target (one, all): ")
+  if s == "one":
+    n = int(input("Track Number: "))
+    if n <= 0 or n > len(files):
+      return
+    t = input(f"New {w}: ")
+    changeone(n - 1, prop, t)
+  elif s == "all":
+    a = input(f"New {w}: ")
+    changeall(prop, a)  
+
+# Rename all file names based on tags
+# Syntax: {tracknumber}_{the_title}
 def rename_files():
   for file in files:
     audio = mutagen.File(file)
@@ -66,6 +88,8 @@ def rename_files():
     p = Path(file)
     p.rename(Path(p.parent, new_name))
 
+# Show tracks to use as reference
+# Show Track, Artist, and Title
 def show_tracks():
   print("")
   for i, file in enumerate(files):
@@ -75,6 +99,7 @@ def show_tracks():
     index = i + 1
     print(f"Track: {index} | Artist: {artist} | Title: {title}")
 
+# Fill missing data on Track, Title, and Artist
 def check_tracks():
   for file in files:
     audio = mutagen.File(file)
@@ -82,7 +107,6 @@ def check_tracks():
     if "tracknumber" not in audio:
       audio["tracknumber"] = "1"
       update_track(audio)
-      changeone("tracknumber")
     if "title" not in audio or audio["title"][0].strip() == "":
       audio["title"] = Path(file).stem.strip()
       update_track(audio)
@@ -90,6 +114,7 @@ def check_tracks():
       audio["artist"] = "Anon"
       update_track(audio)      
 
+# Update track numbers based on the indexes of the files list
 def update_tracknumbers():
   for i, file in enumerate(files):
     audio = mutagen.File(file)
@@ -99,32 +124,43 @@ def update_tracknumbers():
       audio["tracknumber"] = n
       update_track(audio)
 
+# Save track data and show feedback
 def update_track(audio):
   title = audio["title"][0]
   print(f"Updating track: {title}")
   audio.save()
 
+# Change the index of a list item
 def change_index(old_index, new_index):
   files.insert(new_index, files.pop(old_index))
 
+# Change a field on all tracks
 def changeall(field, value):
   for i, file in enumerate(files):
     changeone(i, field, value)
 
+# Change a field on a specific track
 def changeone(index, field, value):
   audio = mutagen.File(files[index])
   audio[field] = value
   update_track(audio)
 
+# Sort by tracknumber data
 def initial_sort():
   global files
   files = sorted(files, key=lambda x: int(mutagen.File(x)["tracknumber"][0]))
 
-files = glob.glob(f"{sys.argv[1]}/*.flac")
+# Main function
+def main() -> None:
+  global files
+  files = glob.glob(f"{sys.argv[1]}/*.flac")
+  check_tracks()
+  initial_sort()
+  update_tracknumbers()
 
-check_tracks()
-initial_sort()
+  while True:
+    show_tracks()
+    show_menu()  
 
-while True:
-  show_tracks()
-  show_menu()
+if __name__ == "__main__":
+  main()  
